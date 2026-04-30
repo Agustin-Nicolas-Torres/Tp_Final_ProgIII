2742f2247e5187c5b203be7a6a1c3e7baee6a216
Agustin-Nicolas-Torres <agustinnicolastorres963@gmail.com>
2026-04-29 21:53:57 -0300

    banners 1.1 - agus

--- abm-clase/src/Componentes/Carrusel.js
+++ abm-clase/src/Componentes/Carrusel.js
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import { imageList } from "./Data";
+import './StyleCarrucel.css';
 
 export default function Banner(){
     const [index, setIndex] = useState(0);
 
     function handleNextClick() {
-        setIndex(index + 1)
+        if (index < imageList.length -1){
+            setIndex(index + 1)
+        }
     }
 
     function handleLeftClick(){
@@ -16,17 +19,27 @@ export default function Banner(){
 
     let BannersImages = imageList[index];
     return(
-        <>
-            <button onClick={handleNextClick}>
-                Next
-            </button>
-            <button onClick={handleLeftClick}>
-                Left 
-            </button>
-            <img
-                src={BannersImages.url}
-                alt= ""
-            />
-        </>
+        <div className="Composiciones">
+            <div className="BNT-IMG">
+            <button className="btn-next" onClick={handleNextClick}> ▶ </button>
+            <button className="btn-left" onClick={handleLeftClick}> ◀ </button>
+            <img src={BannersImages.url} alt= "" />
+            </div>
+                <div style={{textAlign: "center"}}>
+                    {imageList.map((item, pos)=> (
+                        <span
+                            key={pos}
+                                style={{
+                                    height: '10px',
+                                    width: '10px',
+                                    backgroundColor: pos === index ? 'gray' : 'lightgray',
+                                    borderRadius: '50%',
+                                    display: 'inline-block',
+                                    margin: '8px'
+                                    }}
+                        ></span>
+                    ))}
+                </div>
+            </div>
     );
 }
--- abm-clase/src/Componentes/Data.js
+++ abm-clase/src/Componentes/Data.js
@@ -2,5 +2,11 @@ export const imageList = [{
     url: 'Galaxy-A25-y-A15.png'
 }, {
     url:'BannerIphone16.png'
+}, {
+    url:'BannerIphone16.png'
+}, {
+    url:'BannerIphone16.png'
+}, {
+    url:'BannerIphone16.png'
 }
 ];
